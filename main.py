0c33ac814e6a096589f5cd49304242485d06a48d
tusshar17 <tr.jakhara019@gmail.com>
2025-03-25 01:29:16 +0530

    path and query params

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -7,4 +8,29 @@ app = FastAPI()
 async def read_root():
     return {
         'messsage': 'Hello World!'
+    }
+
+
+# using path parameter
+@app.get('/greet/{name}')
+async def greet_name(name: str) -> dict:
+    return {
+        'message': f'Hello {name}!'
+    }
+
+
+# using query parameter
+@app.get('/say-hello')
+async def say_hello(name: str) -> dict:
+    return {
+        'message': f'Hello {name}!'
+    }
+
+
+# using optional query parameter
+@app.get('/greet')
+async def greet_user(name: Optional[str] = 'User', age: int = 0) -> dict:
+    return {
+        'message': f'Hello {name}!',
+        'age': age
     }
