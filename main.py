3512dd1d8d4c2d5ea893842765ee52333b3c72f4
tusshar17 <tr.jakhara019@gmail.com>
2025-03-25 01:55:21 +0530

    Handling headers and using request body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -33,4 +34,35 @@ async def greet_user(name: Optional[str] = 'User', age: int = 0) -> dict:
     return {
         'message': f'Hello {name}!',
         'age': age
+    }
+
+
+# handling headers
+@app.get('/get-headers')
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None)
+):
+    request_headers = {}
+
+    request_headers['Accept'] = accept
+    request_headers['Content-Type'] = content_type
+    request_headers['User-Agent'] = user_agent
+    request_headers['Host'] = host
+
+    return request_headers
+
+
+# schema to create a book
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+# create book using rquest body
+@app.post('/create-book')
+async def create_book(book_data: BookCreateModel):
+    return {
+        'book_data': book_data
     }
